560bb8de07072ab29e1fc54426b9a72f999e3e5e
Forcriun <forcriun@gmail.com>
2018-02-14 23:17:56 +0100

    Creada clase AgendaTelefonica basica que almacena contactos con su nombre y telefono

---
+++ AgendaTelefonica.java
@@ -0,0 +1,41 @@
+import java.util.HashMap;
+
+/**
+ * Clase que representa una guia telefonica muy basica que almacena contactos
+ *  y sus numeros de telefono.
+ *
+ * @author Didac Fernandez Fernandez
+ * @version 14/02/2018
+ */
+public class AgendaTelefonica
+{
+    private HashMap<String,String> agenda;
+
+    /**
+     * Constructor de la clase AgendaTelefonica.
+     */
+    public AgendaTelefonica()
+    {
+        this.agenda = new HashMap<>();
+    }
+
+    /**
+     * Metodo que añade contactos a la agenda telefonica.
+     *
+     * @param  name  El nombre del contacto telefonico
+     * @param  number  El numero del contacto telefonico
+     */
+    public void enterNumber(String name, String number){
+        agenda.put(name,number);
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  name  El nombre del contacto telefonico
+     * @return    El numero del contacto telefonico
+     */
+    public String lookupNumber(String name){        
+        return agenda.get(name);
+    }
+}
